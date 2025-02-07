caafbbb9c1f66c37800b7d60f9b0002b29e37b34
yijordan <yij@uchicago.edu>
2025-02-07 02:24:53 +0000

    wrote prompt text

--- qr.rb
+++ qr.rb
@@ -1,2 +1,11 @@
 # Write your solution here!
-pp "hello world"
+require "rqrcode"
+# qrcode = RQRCode::QRCode.new("SMSTO:5102135718:Hi Mantha! It's Jordan. I'm sending this text through a QR code that I coded in class. Hope you're having fun at sushi :)")
+# png = qrcode.as_png({ :size => 500 })
+# IO.binwrite("sometext.png", png.to_s)
+
+puts "Hi! I'm your QR code generator. I can make you a unique QR code to do one of the following:\n\n"
+  puts "1. Open a URL\n"
+  puts "2. Join a wifi network\n"
+  puts "3. Send a text message\n\n"
+puts "Enter the number of the QR code type you want, or enter 4 to exit."
